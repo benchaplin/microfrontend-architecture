f154e5b96accf0d6f92d5249673cd52aa890d830
Ben Chaplin <benchaplin@protonmail.ch>
2021-06-17 17:25:31 -0400

    Temp rename

---
+++ app3/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/asset-manifest.json", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "asset-manifest.json"));
+});
+
+app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+app.listen(3002, () => console.log("Server running on port 3002..."));
